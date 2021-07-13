8d887d1e5e0b231b00502085813ed747dc487b5a
Eugenio Piazza <Eugenio.Piazza@nasdaq.com>
2021-07-13 20:06:43 +0000

    Added improved customers model

--- models/customers.sql
+++ models/customers.sql
@@ -24,7 +24,7 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(amount)     as lifetime_amount
+        sum(amount)     as lifetime_value
 
     from orders
 
@@ -42,7 +42,7 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_amount
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
 
     from customers
 
